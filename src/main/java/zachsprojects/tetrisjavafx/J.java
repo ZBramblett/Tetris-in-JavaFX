4eeafa40c778c2ea0a5d63e58f1d7357eb464228
Zach <bramb034@d.umn.edu>
2025-04-26 16:12:36 -0500

    Class for J shape

---
+++ src/main/java/zachsprojects/tetrisjavafx/J.java
@@ -0,0 +1,12 @@
+package zachsprojects.tetrisjavafx;
+
+public class J extends Tetrimino{
+    public J(){
+        this.shape = new int[][]{
+                {0, 1, 0, 0},
+                {0, 1, 0, 0},
+                {1, 1, 0, 0},
+                {0, 0, 0, 0}
+        };
+    }
+}
