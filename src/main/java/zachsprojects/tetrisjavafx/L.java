832358ab09dd9957a9ec9e3f4d4e6ede2d83fa8c
Zach <bramb034@d.umn.edu>
2025-04-26 16:11:49 -0500

    Class for L shape

---
+++ src/main/java/zachsprojects/tetrisjavafx/L.java
@@ -0,0 +1,12 @@
+package zachsprojects.tetrisjavafx;
+
+public class L extends Tetrimino{
+    public L(){
+        this.shape = new int[][]{
+                {0, 1, 0, 0},
+                {0, 1, 0, 0},
+                {0, 1, 1, 0},
+                {0, 0, 0, 0}
+        };
+    }
+}
