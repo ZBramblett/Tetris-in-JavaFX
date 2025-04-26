b49d84cabbd5e868dad09d6e6d8e8bc8c0a86be7
Zach <bramb034@d.umn.edu>
2025-04-26 16:10:58 -0500

    Class for Z shape

---
+++ src/main/java/zachsprojects/tetrisjavafx/Z.java
@@ -0,0 +1,12 @@
+package zachsprojects.tetrisjavafx;
+
+public class Z extends Tetrimino{
+    public Z(){
+        this.shape = new int[][]{
+                {1, 1, 0, 0},
+                {0, 1, 1, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}
+        };
+    }
+}
