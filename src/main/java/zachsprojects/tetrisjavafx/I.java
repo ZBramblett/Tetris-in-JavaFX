09e27bbe766a723878c51ee6ff91e0a5a46e3464
Zach <bramb034@d.umn.edu>
2025-04-26 16:06:22 -0500

    Class for I shape

--- src/main/java/zachsprojects/tetrisjavafx/I.java
+++ src/main/java/zachsprojects/tetrisjavafx/I.java
@@ -1,4 +1,18 @@
 package zachsprojects.tetrisjavafx;
 
-public class I {
+public class I extends Tetrimino{
+    
+    public I(){
+        this.shape = new int[][]{
+                {0, 1, 0, 0},
+                {0, 1, 0, 0},
+                {0, 1, 0, 0},
+                {0, 1, 0, 0}
+        };
+    }
+    
+    @Override
+    public int[][] getShape(){
+        return shape;
+    }
 }
