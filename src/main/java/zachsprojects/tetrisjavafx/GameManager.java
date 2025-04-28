b061ee5e2fc001e5ef57edcb46c82b74044a86bb
Zach <bramb034@d.umn.edu>
2025-04-28 09:31:33 -0500

    Added a JavaFX Timeline to move the piece down

--- src/main/java/zachsprojects/tetrisjavafx/GameManager.java
+++ src/main/java/zachsprojects/tetrisjavafx/GameManager.java
@@ -1,32 +1,43 @@
 package zachsprojects.tetrisjavafx;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 public class GameManager extends AnimationTimer {
     
     private GraphicsContext graphicsContext;
     private Canvas canvas;
     
-    public GameManager(Canvas canvas, GraphicsContext gc){
-    this.canvas = canvas;
-    this.graphicsContext = gc;
-    
-    this.canvas.setOnKeyReleased(this::handleKeyPresses);
-    
-    this.canvas.setFocusTraversable(true);
-    
+    public GameManager(Canvas canvas, GraphicsContext gc) {
+        this.canvas = canvas;
+        this.graphicsContext = gc;
+        
+        this.canvas.setOnKeyReleased(this::handleKeyPresses);
+        
+        this.canvas.setFocusTraversable(true);
+        
     }
     
-    TetrisBoard gameBoard = new TetrisBoard(20,10);
+    TetrisBoard gameBoard = new TetrisBoard(20, 10);
     Tetrimino T = new T();
+    
+    Timeline gravity = new Timeline(new KeyFrame(Duration.millis(250), event -> {
+        gameBoard.removeShape(T); T.setCurrentRow(T.getCurrentRow() + 1);})
+    );
+    
+    
     
     @Override
     public void handle(long l) {
         // Game loop
+        gravity.play();
         graphicsContext.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
         
         graphicsContext.setFill(Color.BLACK);
@@ -46,7 +57,6 @@ public class GameManager extends AnimationTimer {
             default: break;
         }
     }
-
     
 }
 
